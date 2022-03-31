ad0bf2aa858a07333c96b9dde6e25c2e8d3854e4
prz23 <panruizheng@zju.edu.cn>
2022-03-31 18:01:07 +0800

    add key-server-fake

--- run.sh
+++ run.sh
@@ -1,20 +1,7 @@
-#!/bin/sh
-
-# Assign an IP address to local loopback
+# Assign an IP address to local loopback 
 ip addr add 127.0.0.1/32 dev lo
 
 ip link set dev lo up
 
-# Add a hosts record, pointing target site calls to local loopback
-echo "127.0.0.1   ip-ranges.amazonaws.com" >> /etc/hosts
-
-touch /app/libnsm.so
-
 # Run traffic forwarder in background and start the server
-./app/server-test 8 50
-
-# shellcheck disable=SC2160
-while true
-do
-  sleep 5000
-done
+./server-test 8 50
